a67fefaf99ac9cb66c00a13c899f559050308226
a733491891-png <a733491891@gmail.com>
2026-06-25 21:15:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_template
 
+app = Flask(__name__, template_folder='templates')
+
+@app.route("/")
+def index():
+    # هذا السطر هو السحر اللي يخلي الموقع يقرأ ملفك الطويل من داخل مجلد templates
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
